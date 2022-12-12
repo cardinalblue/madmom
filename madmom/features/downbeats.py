f39f322b09f9f43756ac7d6bd80d83fe88507d51
Yen-Nan (Maso) Lin <yennanlin.tw@gmail.com>
2022-12-12 11:47:07 +0800

    Fix RNNDownBeatProcessor don't parallel to execute

--- madmom/features/downbeats.py
+++ madmom/features/downbeats.py
@@ -76,7 +76,7 @@ class RNNDownBeatProcessor(SequentialProcessor):
         # define pre-processing chain
         sig = SignalProcessor(num_channels=1, sample_rate=44100)
         # process the multi-resolution spec & diff in parallel
-        multi = ParallelProcessor([])
+        tasks = []
         frame_sizes = [1024, 2048, 4096]
         num_bands = [3, 6, 12]
         for frame_size, num_bands in zip(frame_sizes, num_bands):
@@ -88,7 +88,8 @@ class RNNDownBeatProcessor(SequentialProcessor):
             diff = SpectrogramDifferenceProcessor(
                 diff_ratio=0.5, positive_diffs=True, stack_diffs=np.hstack)
             # process each frame size with spec and diff sequentially
-            multi.append(SequentialProcessor((frames, stft, filt, spec, diff)))
+            tasks.append(SequentialProcessor((frames, stft, filt, spec, diff)))
+        multi = ParallelProcessor(tasks, num_threads=len(tasks))
         # stack the features and processes everything sequentially
         pre_processor = SequentialProcessor((sig, multi, np.hstack))
         # process the pre-processed signal with a NN ensemble
